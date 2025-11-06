5beec84f56baef0e7faee027324615877d0a1810
2junbro <46478661+2junbro@users.noreply.github.com>
2025-11-06 07:16:28 +0000

    커밋테스트2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("🎈 이준형 app")
 st.write(
+    "테스트12345"
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
